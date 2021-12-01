2a0701387d36837299b97ee152c51bda633f4352
shivamvora <shivampatel27102001@gmail.com>
2021-12-01 23:37:42 +0530

    login api

--- controller/auth.js
+++ controller/auth.js
@@ -19,8 +19,29 @@ exports.register = async ( req, res, next ) => {
     }
 }
 
-exports.login = ( req, res, next ) => {
-    res.send( "Login Route" )
+exports.login = async ( req, res, next ) => {
+    const { email, password } = req.body;
+    if ( !email || !password ) {
+        res.status( 400 ).json( { success: false, error: "Please provide email and password " } )
+    }
+
+    try {
+        const user = await User.findOne( { email } ).select( "+password" );
+        if ( !user ) {
+            res.status( 404 ).json( { success: false, error: "Invalid credentials" } )
+        }
+        const isMatch = await user.matchPasswords( password );
+
+        if ( !isMatch ) {
+            res.status( 404 ).json( { success: false, error: "Invalid credentials" } )
+        }
+        res.status( 200 ).json( {
+            success: true,
+            token: "tr34f3443fc"
+        } )
+    } catch ( error ) {
+        res.status( 500 ).json( { success: false, error: error.message } )
+    }
 }
 
 exports.forgotpassword = ( req, res, next ) => {
--- models/User.js
+++ models/User.js
@@ -34,6 +34,12 @@ UserSchema.pre( "save", async function ( next ) {
     next();
 } )
 
+
+UserSchema.methods.matchPasswords = async function ( password ) {
+    return await bcrypt.compare( password, this.password )
+}
+
 const User = mongoose.model( "User", UserSchema );
+
 
 module.exports = User;

9c7b36760ad1962eabb370d5a982987a78ffa7a6
shivamvora <shivampatel27102001@gmail.com>
2021-12-01 23:07:20 +0530

    register api

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const mongoose = require( 'mongoose' );
+const bcrypt = require( 'bcryptjs' );
+
+const UserSchema = new mongoose.Schema( {
+    username: {
+        type: String,
+        required: [true, "Please proivde a username"]
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide a user email"],
+        unique: true,
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            "Please proivde a valid email"
+        ]
+    },
+    password: {
+        type: String,
+        required: [true, "Please add a password"],
+        minlength: 6,
+        select: false
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date
+} );
+
+UserSchema.pre( "save", async function ( next ) {
+    if ( !this.isModified( "password" ) ) {
+        next();
+    }
+    const salt = await bcrypt.genSalt( 10 );
+    this.password = await bcrypt.hash( this.password, salt )
+    next();
+} )
+
+const User = mongoose.model( "User", UserSchema );
+
+module.exports = User;
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 require( 'dotenv' ).config( { path: "./config.env" } )
 const express = require( 'express' );
+const connectDB = require( './config/db' );
+
+
+// Connect DB
+connectDB();
+
 const app = express();
+
 
 app.use( express.json() );
 
@@ -11,6 +18,11 @@ app.use( '/api/auth', require( './routes/auth' ) )
 
 const PORT = process.env.PORT || 5000;
 
-app.listen( PORT, () => {
+const server = app.listen( PORT, () => {
     console.log( `Server running on port ${PORT}` );
+} )
+
+process.on( "unhandledRejection", ( err, promise ) => {
+    console.log( `Logged error : ${err} ` );
+    server.close( () => process.exit( 1 ) );
 } )

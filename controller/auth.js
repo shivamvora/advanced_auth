0afb35571db5792bed9ee1c551cb899353b5e359
shivamvora <shivampatel27102001@gmail.com>
2021-12-03 23:04:33 +0530

    authorization and token setup

--- controller/auth.js
+++ controller/auth.js
@@ -1,4 +1,5 @@
 const User = require( '../models/User' );
+const ErrorResponse = require( '../utils/errorResponse' );
 
 
 exports.register = async ( req, res, next ) => {
@@ -7,38 +8,31 @@ exports.register = async ( req, res, next ) => {
         const user = await User.create( {
             username, email, password
         } )
-        res.status( 201 ).json( {
-            success: true,
-            user: user
-        } )
+        sendToken( user, 201, res );
     } catch ( error ) {
-        res.status( 500 ).json( {
-            success: false,
-            error: error.message
-        } )
+        next( error )
     }
 }
 
 exports.login = async ( req, res, next ) => {
     const { email, password } = req.body;
     if ( !email || !password ) {
-        res.status( 400 ).json( { success: false, error: "Please provide email and password " } )
+        return next( new ErrorResponse( "Please provide an email and password", 400 ) )
     }
 
     try {
         const user = await User.findOne( { email } ).select( "+password" );
         if ( !user ) {
-            res.status( 404 ).json( { success: false, error: "Invalid credentials" } )
+            return next( new ErrorResponse( "Invalid Credentials", 401 ) )
         }
         const isMatch = await user.matchPasswords( password );
 
         if ( !isMatch ) {
-            res.status( 404 ).json( { success: false, error: "Invalid credentials" } )
+            return next( new ErrorResponse( "Invalid Credentials", 401 ) )
+
         }
-        res.status( 200 ).json( {
-            success: true,
-            token: "tr34f3443fc"
-        } )
+        sendToken( user, 200, res );
+
     } catch ( error ) {
         res.status( 500 ).json( { success: false, error: error.message } )
     }
@@ -50,4 +44,10 @@ exports.forgotpassword = ( req, res, next ) => {
 
 exports.resetpassword = ( req, res, next ) => {
     res.send( "Reset password Route" )
+}
+
+const sendToken = ( user, statusCode, res ) => {
+    const token = user.getSignedToken();
+    res.status( statusCode ).json( { success: true, token } )
+
 }
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require( 'dotenv' ).config( { path: "./config.env" } )
 const express = require( 'express' );
 const connectDB = require( './config/db' );
+const errorHandler = require( './middleware/error' );
 
 
 // Connect DB
@@ -14,7 +15,8 @@ app.use( express.json() );
 
 app.use( '/api/auth', require( './routes/auth' ) )
 
-
+// Error Handler
+app.use( errorHandler );
 
 const PORT = process.env.PORT || 5000;
 

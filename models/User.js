af4d54f6cfa070427d760a0ca89dcaea4be0efa9
shivamvora <shivampatel27102001@gmail.com>
2021-12-04 20:49:49 +0530

    send email setup done

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const crypto = require( 'crypto' );
 const mongoose = require( 'mongoose' );
 const bcrypt = require( 'bcryptjs' );
 const jwt = require( 'jsonwebtoken' );
@@ -42,6 +43,15 @@ UserSchema.methods.matchPasswords = async function ( password ) {
 
 UserSchema.methods.getSignedToken = function () {
     return jwt.sign( { id: this._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRE } )
+}
+
+UserSchema.methods.getResetPasswordToken = function () {
+    const resetToken = crypto.randomBytes( 20 ).toString( "hex" );
+
+    this.resetPasswordToken = crypto.createHash( "sha256" ).update( resetToken ).digest( "hex" );
+
+    this.resetPasswordExpire = Date.now() + 10 * ( 60 * 1000 );
+    return resetToken;
 }
 
 const User = mongoose.model( "User", UserSchema );
